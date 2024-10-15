a83f954e8886fa194ea0f0c10d627bf94843f71a
HyperionU <108021531+HyperionU@users.noreply.github.com>
2024-10-15 21:08:20 +0000

    Add mathlib

--- Game/Metadata.lean
+++ Game/Metadata.lean
@@ -1,16 +1,9 @@
 import GameServer.Commands
 
--- import Mathlib.Tactic.Common
+import Game.NatHU.Definition
 
-/-! Use this file to add things that should be available in all levels.
+import Game.Docs.Definition
+import Game.Docs.Tactics
 
-For example, this demo imports the mathlib tactics
-
-*Note*: As long as `Game.lean` exists and ends with the `MakeGame` command,
-you are completely free how you structure your lean project, this is merely
-a suggestion.
-
-*Bug*: However, things are bugged out if the levels of different worlds are imported
-in a random order. Therefore, you should keep the structure of one file lean file per world
-that imports all its levels.
--/
+import Game.Tactic.rfl
+import Game.Tactic.rw
