fe59c826e3e685961e6aa6307aa166df65cf0f93
HyperionU <108021531+HyperionU@users.noreply.github.com>
2024-10-21 16:52:30 +0000

    add Algorithm

--- Game.lean
+++ Game.lean
@@ -3,6 +3,7 @@ import Game.Levels.Addition
 import Game.Levels.Multiplication
 import Game.Levels.Implication
 import Game.Levels.Power
+import Game.Levels.Algorithm
 
 -- Here's what we'll put on the title screen
 Title "Intro to Lean"
---
+++ Game/Levels/Algorithm.lean
@@ -0,0 +1,20 @@
+import Game.Levels.Algorithm.L01_ladcomm
+import Game.Levels.Algorithm.L02_addalgo_I
+import Game.Levels.Algorithm.L03_addalgo_II
+import Game.Levels.Algorithm.L04_addalgo_III
+import Game.Levels.Algorithm.L05_pred
+import Game.Levels.Algorithm.L06_iszero
+import Game.Levels.Algorithm.L07_succnesucc
+import Game.Levels.Algorithm.L08_decide_I
+import Game.Levels.Algorithm.L09_decide_II
+
+World "Algorithm"
+Title "Algorithm World"
+
+Introduction
+"
+Proofs like $2+2=4$ and $a+b+c+d+e=e+d+c+b+a$ are very tedious to do by hand.
+In Algorithm World we learn how to get the computer to do them for us.
+
+Click on \"Start\" to proceed.
+"
